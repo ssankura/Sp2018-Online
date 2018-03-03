3f1e0df129b17b37b6abe7868709d04b9fc9c9a9
Andy Miles <akmiles@icloud.com>
2018-03-03 10:16:36 -0800

    newly structured

--- personjob_learning_v3_p1.py
+++ personjob_learning_v3_p1.py
@@ -1,58 +1,66 @@
 """
     Learning persistence with Peewee and sqlite
-    delete the database to start over 
+    delete the database to start over
         (but running this program does not require it)
-        
-    Person:
-        1. insert records
-        2. display all records
-        3. show transactions
-        4. show error checking
-        5. show logging (to explain what's going on)  
-        
 """
-
+import logging
 from personjob_model import *
 
-logger.info('Working with Person class')
-logger.info('Note how I use constants and a list of tuples as a simple schema')
-logger.info('Normally you probably will have prompted for this from a user')
+def populate_db():
+    """
+    add person data to database
+    """
 
-PERSON_NAME = 0
-LIVES_IN_TOWN = 1
-NICKNAME = 2
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
 
-people = [
-    ('Andrew', 'Sumner', 'Andy'),
-    ('Peter', 'Seattle', None),
-    ('Susan', 'Boston', 'Beannie'),
-    ('Pam', 'Coventry', 'PJ'),
-    ('Steven', 'Colchester', None), 
-    ]
+    database = SqliteDatabase('personjob.db')
 
-logger.info('Creating Person records: iterate through the list of tuples')
-logger.info('Prepare to explain any errors with exceptions')
-logger.info('and the transaction tells the database to rollback on error')
+    logger.info('Working with Person class')
+    logger.info('Note how I use constants and a list of tuples as a simple schema')
+    logger.info('Normally you probably will have prompted for this from a user')
 
-for person in people:
+    PERSON_NAME = 0
+    LIVES_IN_TOWN = 1
+    NICKNAME = 2
+
+    people = [
+        ('Andrew', 'Sumner', 'Andy'),
+        ('Peter', 'Seattle', None),
+        ('Susan', 'Boston', 'Beannie'),
+        ('Pam', 'Coventry', 'PJ'),
+        ('Steven', 'Colchester', None),
+        ]
+
+    logger.info('Creating Person records: iterate through the list of tuples')
+    logger.info('Prepare to explain any errors with exceptions')
+    logger.info('and the transaction tells the database to fail on error')
+
     try:
-        with database.transaction():
-            new_person = Person.create(
-                    person_name = person[PERSON_NAME],
-                    lives_in_town = person[LIVES_IN_TOWN],
-                    nickname = person[NICKNAME])
-            new_person.save()
-            logger.info('Database add successful')
+        database.connect()
+        database.execute_sql('PRAGMA foreign_keys = ON;')
+        for person in people:
+            with database.transaction():
+                new_person = Person.create(
+                        person_name = person[PERSON_NAME],
+                        lives_in_town = person[LIVES_IN_TOWN],
+                        nickname = person[NICKNAME])
+                new_person.save()
+                logger.info('Database add successful')
+
+        logger.info('Print the Person records we saved...')
+        for saved_person in Person:
+            logger.info(f'{saved_person.person_name} lives in {saved_person.lives_in_town} ' +\
+                f'and likes to be known as {saved_person.nickname}')
 
     except Exception as e:
         logger.info(f'Error creating = {person[PERSON_NAME]}')
         logger.info(e)
-        logger.info('See how the datbase protects our data')
+        logger.info('See how the database protects our data')
 
-logger.info('Read and print all Person records we created...')
+    finally:
+        logger.info('database closes')
+        database.close()
 
-for person in Person:
-    logger.info(f'{person.person_name} lives in {person.lives_in_town} ' +\
-        f'and likes to be known as {person.nickname}')
-
-database.close()
+if __name__ == '__main__':
+    populate_db()
