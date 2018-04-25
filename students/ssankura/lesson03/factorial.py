fb43abc7e17b3585e0a4a22112452897c3c81063
ssankura <sireesha@gmail.com>
2018-04-25 08:59:39 +0530

    Updated Factorial - removed a base condition for n=1 - using recursion for n>=1

--- students/ssankura/lesson03/factorial.py
+++ students/ssankura/lesson03/factorial.py
@@ -10,10 +10,11 @@ def factorial(n):
 	'''
 	if n<0: raise ValueError("Invalid Input for Factorial {}".format(n))
 	if n==0: return 1
-	if n==1: return 1
-	if n>=2: return (n * factorial(n-1))
+	if n>=1: return (n * factorial(n-1))
 
 if __name__ == '__main__':
-	print(factorial(10))
-
-
+	print ("Factorial of {} is {}".format(0,factorial(0)))
+	print ("Factorial of {} is {}".format(1,factorial(1)))
+	print ("Factorial of {} is {}".format(5,factorial(5)))
+	print ("Factorial of {} is {}".format(10,factorial(10)))
+	print ("Factorial of {} is {}".format(100,factorial(100)))
