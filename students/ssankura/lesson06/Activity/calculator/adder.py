8551ad8619b9bc84d40fc868086a90f230bdb47c
ssankura <sireesha@gmail.com>
2018-05-17 23:05:08 +0530

    Lesson01 - Activity1

---
+++ students/ssankura/lesson06/Activity/calculator/adder.py
@@ -0,0 +1,15 @@
+"""
+Lesson06 - Activity 1
+Author: Sireesha Sankuratripati
+Implementation of Calculator - Adder
+"""
+
+
+class Adder(object):
+    """ Implementation of the Adder Class """
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """calculate method which performs division operation
+           on the 2 operands specified in input"""
+        return operand_1 + operand_2
---
+++ students/ssankura/lesson06/Activity/calculator/exceptions.py
@@ -0,0 +1,11 @@
+"""
+Lesson06 - Activity 1
+Author: Sireesha Sankuratripati
+Implementation of Calculator - Exceptions class
+"""
+
+
+class InsufficientOperands(Exception):
+    """ Implementation of the InsufficientOperands Exception Class """
+
+    pass
